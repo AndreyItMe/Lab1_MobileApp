2dd7a15b186512ccc372a210c1ae50baa868ede2
andrey Ivanov <andreyvorobey.play@gmail.com>
2025-03-07 12:07:16 -0700

    Lesson 6

---
+++ app/src/main/java/com/example/bookstoreapp/ui/login/LoginScreen.kt
@@ -0,0 +1,149 @@
+package com.example.bookstoreapp.ui.login
+
+import android.util.Log
+import android.widget.Toast
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.google.firebase.appcheck.internal.util.Logger.TAG
+import com.google.firebase.auth.EmailAuthProvider
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+import kotlin.math.sign
+
+@Composable
+fun LoginScreen() {
+    val auth = Firebase.auth
+
+    val emailState = remember {
+        mutableStateOf("")
+    }
+
+    val passwordState = remember {
+        mutableStateOf("")
+    }
+    Log.d("MyLog", "User email: ${auth.currentUser?.email}")
+    Column(modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        TextField(value = emailState.value, onValueChange = {
+            emailState.value = it
+        })
+        Spacer(modifier = Modifier.height(10.dp))
+
+        TextField(value = passwordState.value, onValueChange = {
+            passwordState.value = it
+        })
+        Spacer(modifier = Modifier.height(10.dp))
+
+        Button(onClick = {
+            signIn(auth, emailState.value, passwordState.value)
+        }) {
+            Text(text = "Sign In")
+        }
+
+        Button(onClick = {
+            signUp(auth, emailState.value, passwordState.value)
+        }) {
+            Text(text = "Sign Up")
+        }
+
+        Button(onClick = {
+            signOut(auth)
+        }) {
+            Text(text = "Sign out")
+        }
+
+        Button(onClick = {
+            deleteAccount(auth, emailState.value, passwordState.value)
+        }) {
+            Text(text = "Delete account")
+        }
+    }
+}
+
+private fun signUp(auth: FirebaseAuth, email: String, password: String){
+    auth.createUserWithEmailAndPassword(email, password)
+        .addOnCompleteListener{ task ->
+            if (task.isSuccessful) {
+            //if (it.isSuccessful) {
+                // Sign in success, update UI with the signed-in user's information
+                Log.d("MyLog", "createUserWithEmail:success")
+                val user = auth.currentUser
+                //updateUI(user)
+            } else {
+                // If sign in fails, display a message to the user.
+                Log.d("MyLog", "createUserWithEmail:failure", task.exception)
+                /*
+                Toast.makeText(
+                    baseContext,
+                    "Authentication failed.",
+                    Toast.LENGTH_SHORT,
+                ).show()
+                 */
+                //updateUI(null)
+            }
+        }
+}
+private fun signIn(auth: FirebaseAuth, email: String, password: String){
+    auth.signInWithEmailAndPassword(email, password)
+        .addOnCompleteListener{ task ->
+            if (task.isSuccessful) {
+                //if (it.isSuccessful) {
+                // Sign in success, update UI with the signed-in user's information
+                Log.d("MyLog", "Sign in:success")
+                val user = auth.currentUser
+                //updateUI(user)
+            } else {
+                // If sign in fails, display a message to the user.
+                Log.d("MyLog", "Sing in:failure", task.exception)
+                /*
+                Toast.makeText(
+                    baseContext,
+                    "Authentication failed.",
+                    Toast.LENGTH_SHORT,
+                ).show()
+                 */
+                //updateUI(null)
+            }
+        }
+}
+
+private fun signOut(auth: FirebaseAuth){
+    auth.signOut()
+    Log.d("MyLog", "Sign Out: success")
+}
+
+private fun deleteAccount(auth: FirebaseAuth, email: String, password: String){
+    val credential = EmailAuthProvider.getCredential(email, password)
+    auth.currentUser?.reauthenticate(credential)?.addOnCompleteListener {
+        if(it.isSuccessful){
+            auth.currentUser?.delete()?.addOnCompleteListener {
+                if(it.isSuccessful){
+                    Log.d("MyLog", "delete Account: success")
+                } else
+                {
+                    Log.d("MyLog", "delete Account: failure")
+                }
+            }
+        }else{
+            Log.d("MyLog", "reauthenticate: failure")
+        }
+    }
+
+
+}
